708d2ede5122b3de375f60ba86f947349d06f6e2
Kirill <pomogajko@gmail.com>
2015-12-25 21:08:27 +0100

    new folder scripts with topcran calculation

---
+++ _R/scripts/topcran.R
@@ -0,0 +1,37 @@
+library(rvest)
+library(dplyr)
+library(cranlogs)
+library(pbapply)
+library(ggplot2)
+library(scales)
+
+berries <- read_html("http://dirk.eddelbuettel.com/cranberries/2015/")
+titles <- berries %>% html_nodes("b") %>% html_text
+new <- titles[grepl("^New package", titles)] %>% 
+  gsub("^New package (.*) with initial .*", "\\1", .) %>% unique
+
+logs <- pblapply(new, function(x) {
+  down <- cran_downloads(x, from = "2015-01-01")$count 
+  if(sum(down) > 0) {
+    public <- down[which(down > 0)[1]:length(down)]
+  } else {
+    public <- 0
+  }
+  return(data.frame(package = x, sum = sum(down), avg = mean(public)))
+})
+
+logs <- do.call(rbind, logs) 
+
+top <- logs %>% 
+  unique %>% 
+  arrange(-avg) %>% 
+  head(20)
+
+top %>% ggplot() + 
+  aes (x = package, y = avg) + 
+  geom_bar(stat = "identity") + 
+  scale_x_discrete(limits = rev(top$package)) +
+  scale_y_continuous(labels = comma) +
+  coord_flip() + 
+  ylab("Downloads from R-Studio CRAN mirror") + xlab("") +
+  ggtitle("Top 20 new CRAN packages in 2015")
